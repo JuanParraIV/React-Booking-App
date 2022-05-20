4024442c7114b5ccf43ecfb2ade0db8ef6cad8cc
JuanParraIV <jmparra1993@gmail.com>
2022-05-20 18:30:05 -0500

    Connection to mongo finished

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 dotenv.config();
 
-app.listen(8800, () => {
-    console.log('Server is running on port 8800');
-    }
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log(("Connected to MongoDB"));
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected of MongoDB");
+}
 );
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+}
+);
+app.listen(8800, () => {
+  connect()
+  console.log("Server is running on port 8800");
+});
